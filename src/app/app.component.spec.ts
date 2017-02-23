76a9d7bb1230d7037b5d1d13a7bc9386c19e82ff
Anton <anton.arboleda@gmail.com>
2017-02-23 02:12:10 -0800

    [write] - test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -31,4 +31,5 @@ describe('AppComponent', () => {
     const compiled = fixture.debugElement.nativeElement;
     expect(compiled.querySelector('h1').textContent).toContain('app works!');
   }));
+
 });
