c7503a05f8d5328e195407f18427416bfe781f18
Anton <anton.arboleda@gmail.com>
2017-02-23 01:37:02 -0800

    [update] - filter logic

--- src/app/pipes/filter.pipe.ts
+++ src/app/pipes/filter.pipe.ts
@@ -7,16 +7,30 @@ export class FilterPipe implements PipeTransform {
 
   transform(comments: any, input: any): any {
     // if no searchInput, return all comments
-    if (input === undefined) return comments;
+    if (input === undefined) {
+      return comments;
+    };
+    if (input.length === 0) {
+      return comments;
+    };
+    if ( typeof comments === undefined) {
+      return;
+    }
+
     if (Number(input) >= 0) {
       return comments.filter((comment) => {
         return comment.id === Number(input);
       })
-    } else {
+    } else if (typeof input === 'string') {
       return comments.filter((comment) => {        
-        return comment.email.toLowerCase().indexOf(input.toLowerCase()) >= 0;
+        return comments = comment.email.toLowerCase().indexOf(input.toLowerCase()) >= 0 || 
+        comment.body.toLowerCase().indexOf(input.toLowerCase()) >= 0 || 
+        comment.name.toLowerCase().indexOf(input.toLowerCase()) >= 0;
       })
+    } else if (input.length > 0 && comments.length === 0) {
+      console.log('hello world')
     }
+
   }
 
 
